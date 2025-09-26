64927916d3a1d37d290abc87c357b4ab7b28eb71
Yunik-D <diogolima5328@gmail.com>
2025-09-26 00:52:39 -0300

    Converte idade string > number. Corrige ortografia e melhora os styles.

--- app/index.tsx
+++ app/index.tsx
@@ -4,19 +4,20 @@ import { StyleSheet, Text, TextInput, View } from "react-native";
 export default function Home(){
     const [name, setName] = useState(''); //useState do nome (UseState é o controlador de estado).
     const [age, setAge] = useState('');
+    const ageN = parseInt(age);
 
     return(
         <View style={styles.body}> 
-        <TextInput placeholder = "Wie haiße sie?"
-         onChangeText={setName}/>
+        <TextInput placeholder = "Wie heiße Sie?"
+         onChangeText={setName} style={styles.input}/>
             <Text style={styles.title}>
-                Hallo, Ich haiße {name}!
+                Hallo, Ich heiße {name}!
                 </Text>
                 
         <TextInput placeholder = "Wie alt bist du?"
-         onChangeText={setAge}/>
+         onChangeText={setAge} keyboardType="numeric" style={styles.input}/>
             <Text style={styles.title}>
-                 Ich bin {age} Jahre alt!
+                 Ich bin {ageN} Jahre alt!
                 </Text>
             
         </View>
@@ -25,10 +26,21 @@ export default function Home(){
 
 const styles = StyleSheet.create({
     body: {
-        backgroundColor: "red",
-        padding: "auto"
+        flex: 1,
+        backgroundColor: "cyan",
+        padding: 20
     },
     title: {
-        fontSize: 19
-    }
+        fontSize: 25,
+        fontFamily: "Rosemary",
+        marginBottom: 15
+    },
+      input: {
+    borderWidth: 1,
+    borderColor: "#333",
+    borderRadius: 8,
+    padding: 10,
+    marginBottom: 15,
+    backgroundColor: "#fff"
+      }
 })
